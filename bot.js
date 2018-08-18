b371911647da8d9eac65100c653d7792ed262d0a
captaincrazybro <33706558+captaincrazybro@users.noreply.github.com>
2018-08-18 15:56:09 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -41,6 +41,7 @@ bot.on("message", async message => {
 	
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
     if(commandfile) commandfile.run(bot,message,args,cmd,fs);
+	
 
 });
 		
